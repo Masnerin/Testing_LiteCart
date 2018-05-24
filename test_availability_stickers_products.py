d52c4ddab11942cf37460ffbc976d0b1339c4bcc
Sergey Davydov <32953372+Masnerin@users.noreply.github.com>
2018-05-24 09:55:48 +0300

    Presence of stickers

---
+++ test_availability_stickers_products.py
@@ -0,0 +1,34 @@
+# Задания по тестированию LiteCart.
+# Проверка наличия стикеров у всех товаров
+# на главной странице сайта
+# в учебном приложении LiteCart.
+
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+class MasnerinOrder(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_orders_in_masnerin_com(self):
+        driver = self.driver
+        driver.get("http://localhost/litecart/en/")
+        self.driver.maximize_window()
+        self.assertIn("My LiteCart", driver.title)
+
+        blocks = driver.find_elements_by_css_selector('a.link[data-toggle="lightbox"]')
+        print("\nНайдено", len(blocks), "блоков.")
+
+        for block in blocks:
+            try:
+                sticker = block.find_element_by_css_selector('div.sticker')
+            except NoSuchElementException:
+                print("В блоке:", block, "нет стикера!")
+            print("В блоке:", block, "есть стикер.")
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
