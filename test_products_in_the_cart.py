367af09aafb5aa18993ba13e9ee3794ad9af4e0f
Sergey Davydov <32953372+Masnerin@users.noreply.github.com>
2018-06-14 10:50:16 +0300

    Products in the cart

--- test_products_in_the_cart.py
+++ test_products_in_the_cart.py
@@ -9,6 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
 
 
 class ProductsInTheCartTesting(unittest.TestCase):
@@ -18,42 +19,33 @@ class ProductsInTheCartTesting(unittest.TestCase):
 
     def test_products_in_the_cart(self):
         driver = self.driver
-        wait = WebDriverWait(driver, 10)
+        driver.wait = WebDriverWait(driver, 10)
 
 # Вход на главную страницу
         driver.get("http://localhost/litecart")
         self.assertIn("My LiteCart", driver.title)
 
 # Выбор товаров
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.link[title="Green Duck"]'))).click()
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="add_cart_product"]'))).click()
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'span.quantity'), '1'))
-        driver.get("http://localhost/litecart")
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.link[title="Red Duck"]'))).click()
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="add_cart_product"]'))).click()
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'span.quantity'), '2'))
-        driver.get("http://localhost/litecart")
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.link[title="Blue Duck"]'))).click()
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="add_cart_product"]'))).click()
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'span.quantity'), '3'))
+        for j in range(1, 4):
+            driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'li.product'))).click()
+            size = driver.find_elements_by_css_selector('select')
+            if len(size) != 0:
+                Select(driver.find_element_by_css_selector('select')).select_by_index(1)
+            driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[name="add_cart_product"]'))).click()
+            driver.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'span.quantity'), str(j)))
 
 # Удаление товаров из карзины
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.link'))).click()
-        element = driver.find_element_by_css_selector('tr.footer')
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[value=Remove]'))).click()
-        element = driver.find_element_by_css_selector('tr.footer')
-        wait.until(EC.staleness_of(element))
-        element = driver.find_element_by_css_selector('tr.footer')
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[value=Remove]'))).click()
-        wait.until(EC.staleness_of(element))
-        element = driver.find_element_by_css_selector('tr.footer')
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[value=Remove]'))).click()
-        wait.until(EC.staleness_of(element))
-
-        driver.get("http://localhost/litecart")
+        driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.link'))).click()
+        number_product = driver.find_elements_by_css_selector('li.shortcut')
+        for i in range(0, len(number_product)):
+            element = driver.find_elements_by_css_selector('table.dataTable.rounded-corners tr')
+            a = len(element)
+            driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[value=Remove]'))).click()
+            driver.wait.until(EC.staleness_of(element[a - 1]))
 
     def tear_down(self):
         driver.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
